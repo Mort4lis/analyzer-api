c1f34719970d62292befc6cd534efb44b5c50833
Pavel Korchagin <mortalis94@gmail.com>
2020-09-11 00:20:40 +0300

    Init commit

---
+++ analyzer/__init__.py
@@ -0,0 +1,19 @@
+"""
+REST API сервис, анализирующий рынок промо-акций.
+Пример решения вступительного испытания 2019 г. в Школу бэкенд-разработки Яндекса.
+"""
+
+__author__ = 'Pavel Korchagin'
+__maintainer__ = __author__
+
+__email__ = 'mortalis94@gmail.com'
+__license__ = 'MIT'
+__version__ = '0.0.1'
+
+__all__ = (
+    '__author__',
+    '__email__',
+    '__license__',
+    '__maintainer__',
+    '__version__'
+)
