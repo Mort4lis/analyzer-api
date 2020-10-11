e50ad5ea832a740d389aa816ab84f6a719a3643a
Pavel Korchagin <mortalis94@gmail.com>
2020-10-11 17:44:57 +0300

    Implemented citizen and import schemas

---
+++ analyzer/api/schemas.py
@@ -0,0 +1,73 @@
+from marshmallow import Schema, validates_schema
+from marshmallow.fields import Int, Str, Date, Nested, List
+from marshmallow.validate import Range, Length, OneOf, ValidationError
+
+from analyzer.db.schema import Gender
+from analyzer.utils.consts import DATE_FORMAT
+
+POSITIVE_INT = Range(min=0)
+BASIC_STRING_LENGTH = Length(min=1, max=256)
+
+
+class CitizenSchema(Schema):
+    citizen_id = Int(validate=POSITIVE_INT, required=True)
+    name = Str(validate=BASIC_STRING_LENGTH, required=True)
+    gender = Str(validate=OneOf([gender.name for gender in Gender]), required=True)
+    birth_date = Date(format=DATE_FORMAT, required=True)
+    town = Str(validate=BASIC_STRING_LENGTH, required=True)
+    street = Str(validate=BASIC_STRING_LENGTH, required=True)
+    building = Str(validate=BASIC_STRING_LENGTH, required=True)
+    apartment = Int(validate=POSITIVE_INT, required=True)
+    relatives = List(Int(validate=POSITIVE_INT), required=True)
+
+
+class ImportRequestSchema(Schema):
+    citizens = Nested(CitizenSchema, many=True, required=True, validate=Length(max=1000))
+
+    @validates_schema
+    def validate_unique_citizen_id(self, data: dict, **_) -> None:
+        """
+        Валидация на уникальность id-шников жителей в рамках выгрузки.
+
+        :param data: данные схемы
+        """
+        unique_ids = set()
+        for citizen in data['citizens']:
+            if citizen['citizen_id'] in unique_ids:
+                raise ValidationError(
+                    'citizen_id {0!r} is not unique'.format(citizen['citizen_id'])
+                )
+
+            unique_ids.add(citizen['citizen_id'])
+
+    @validates_schema
+    def validate_relatives(self, data: dict, **_) -> None:
+        """
+        Валидация родственников.
+
+        Если у родственника #1 есть родственник #2,
+        то и родственника #2 должен быть родственник #1.
+
+        :param data: данные схемы
+        """
+        relatives_map = {
+            citizen['citizen_id']: set(citizen['relatives'])
+            for citizen in data['citizens']
+        }
+
+        for citizen_id, relatives in relatives_map.items():
+            for relative_id in relatives:
+                if citizen_id not in relatives_map.get(relative_id, set()):
+                    raise ValidationError(
+                        'citizen_id {0!r} does not have relation with {1!r}'.format(
+                            relative_id, citizen_id
+                        )
+                    )
+
+
+class ImportIdSchema(Schema):
+    import_id = Int(required=True)
+
+
+class ImportResponseSchema(Schema):
+    data = Nested(ImportIdSchema, required=True)
